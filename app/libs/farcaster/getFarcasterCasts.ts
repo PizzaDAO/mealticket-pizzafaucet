3e7ab428e850ba78e72e6ada896d6ee15f8e1293
Will Hay <20303031+rocketman-21@users.noreply.github.com>
2024-07-22 19:23:54 -0700

    get last 300 casts

--- app/libs/farcaster/getFarcasterCasts.ts
+++ app/libs/farcaster/getFarcasterCasts.ts
@@ -1,15 +1,28 @@
 import { unstable_cache } from "next/cache";
-import { farcaster } from "./client";
+import { CastWithInteractions, farcaster } from "./client";
 
 export const getChannelCasts = unstable_cache(
   async (channelId: string) => {
-    const feed = await farcaster.fetchFeed("filter", {
-      filterType: "channel_id",
-      limit: 100,
-      channelId,
-    });
+    let i = 0;
+    let cursor: undefined | string = undefined;
+    let allCasts: CastWithInteractions[] = [];
 
-    return feed.casts;
+    while (i < 3) {
+      const feed = await farcaster.fetchFeed("filter", {
+        filterType: "channel_id",
+        limit: 100,
+        channelId,
+        cursor: cursor || undefined,
+      });
+
+      allCasts = allCasts.concat(feed.casts);
+      cursor = feed.next.cursor || undefined;
+      if (cursor === undefined) break;
+
+      i++;
+    }
+
+    return allCasts;
   },
   undefined,
   { revalidate: 60, tags: ["feed"] },
