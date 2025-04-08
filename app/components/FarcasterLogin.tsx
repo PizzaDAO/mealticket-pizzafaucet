ff99ed56845824bd84b4ad9f7d54722ad6d6291a
Godwin <godwinaquinas@gmail.com>
2025-04-08 19:35:32 +0100

    debug false signer

--- app/components/FarcasterLogin.tsx
+++ app/components/FarcasterLogin.tsx
@@ -78,7 +78,7 @@ export const FarcasterLogin = ({ setLoggedIn, toggle, setToggle }: Props) => {
       if (!signInUrl && toggle)
          (async () => {
             try {
-               const signerRes = (await (await fetch("/api/register-signer")).json()).signer
+               const signerRes = (await (await fetch("/api/register-signer", { cache: 'no-store' })).json()).signer
                setSigner(signerRes)
                setSignInUrl(signerRes?.signer_approval_url || "")
                setLoading(false)
