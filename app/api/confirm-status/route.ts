560dd46251cc714b1ff2e4c561026b7c8f710b85
Godwin <godwinaquinas@gmail.com>
2025-05-18 22:24:17 +0100

    add status update failed message

--- app/api/confirm-status/route.ts
+++ app/api/confirm-status/route.ts
@@ -5,7 +5,6 @@ import { farcaster } from "../../libs/farcaster/client";
 export async function POST(req: NextRequest) {
    try {
       const { signer } = await req.json()
-
       const data = await farcaster.lookupSigner({ signerUuid: signer })
       return NextResponse.json({ signer: data }, { status: 200 });
    } catch (error) {
--- app/components/MemberStatusModal.tsx
+++ app/components/MemberStatusModal.tsx
@@ -24,6 +24,9 @@ export const MemberStatusModal = ({ channelId, toggle, setToggle, castData, isMe
    const [status, setStatus] = useState("")
    const [success, setSuccess] = useState(false)
 
+   const [statusUpdateFailed, setStatusUpdateFailed] = useState(false)
+   const [statusMsg, setStatusMsg] = useState('')
+
    useEffect(() => {
       if (toggle && isMember)
          (async () => {
@@ -43,8 +46,11 @@ export const MemberStatusModal = ({ channelId, toggle, setToggle, castData, isMe
          method: 'PUT', body: JSON.stringify(reqData)
       })
       const { success, message }: OperationResponse = await res.json()
+      if (!success) {
+         setStatusUpdateFailed(true)
+         setStatusMsg(message ?? "failed to update member")
+      }
       if (success) {
-         console.log(message)
          await checkMemberStatus(fid ?? 0)
       }
    }
@@ -91,23 +97,29 @@ export const MemberStatusModal = ({ channelId, toggle, setToggle, castData, isMe
          {
             !isMember &&
             <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
-               <DialogPanel className="w-full flex flex-col items-center gap-4 max-w-lg rounded-xl border bg-yellow-50 p-5 font-sans lg:p-8">
-                  <DialogTitle className="w-full text-left font-display text-2xl font-bold">
-                     Accept Pizzafaucet Membership Invitation
-                  </DialogTitle>
-                  <div className="flex gap-4">
-                     <button
-                        className="block rounded-3xl border-2 border-black/50 bg-green-200 px-8 pb-2.5 pt-3.5 text-center font-display text-xl font-bold uppercase text-black shadow-lg duration-100 ease-in-out hover:bg-green-300 hover:text-black" onClick={acceptInvite}
-                     >
-                        Accept
-                     </button>
-                     <button
-                        className="block rounded-3xl border-2 border-black/50 bg-red-200 px-8 pb-2.5 pt-3.5 text-center font-display text-xl font-bold uppercase text-black shadow-lg duration-100 ease-in-out hover:bg-red-300 hover:text-black" onClick={closeModal}
-                     >
-                        Cancel
-                     </button>
-                  </div>
-               </DialogPanel>
+               {  !statusUpdateFailed && 
+                  <DialogPanel className="w-full flex flex-col items-center gap-4 max-w-lg rounded-xl border bg-yellow-50 p-5 font-sans lg:p-8">
+                     <DialogTitle className="w-full text-left font-display text-2xl font-bold">
+                        Accept Pizzafaucet Membership Invitation
+                     </DialogTitle>
+                     <div className="flex gap-4">
+                        <button
+                           className="block rounded-3xl border-2 border-black/50 bg-green-200 px-8 pb-2.5 pt-3.5 text-center font-display text-xl font-bold uppercase text-black shadow-lg duration-100 ease-in-out hover:bg-green-300 hover:text-black" onClick={acceptInvite}
+                        >
+                           Accept
+                        </button>
+                        <button
+                           className="block rounded-3xl border-2 border-black/50 bg-red-200 px-8 pb-2.5 pt-3.5 text-center font-display text-xl font-bold uppercase text-black shadow-lg duration-100 ease-in-out hover:bg-red-300 hover:text-black" onClick={closeModal}
+                        >
+                           Cancel
+                        </button>
+                     </div>
+                  </DialogPanel>
+               }
+               {
+                  statusUpdateFailed && 
+                  <div>{ statusMsg }</div>
+               }
             </div>
          }
          {
