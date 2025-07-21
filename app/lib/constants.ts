56aaf21914c90538c79b61661069de22e613278b
Godwin <godwinaquinas@gmail.com>
2025-07-21 20:18:51 +0100

    update frame manifest

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,4 +1,4 @@
-export const APP_URL = process.env.NEXT_PUBLIC_URL!;
+export const APP_URL = `https://${process.env.NEXT_PUBLIC_URL!}`;
 export const APP_NAME = process.env.NEXT_PUBLIC_FRAME_NAME;
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_FRAME_DESCRIPTION;
 export const APP_PRIMARY_CATEGORY = process.env.NEXT_PUBLIC_FRAME_PRIMARY_CATEGORY;
