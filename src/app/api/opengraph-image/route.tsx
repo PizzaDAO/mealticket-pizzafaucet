04c286ce640d3155e41b6c466c94812d7ba60406
Godwin <godwinaquinas@gmail.com>
2025-08-11 20:49:41 +0100

    fix open-graph image

--- src/app/api/opengraph-image/route.tsx
+++ src/app/api/opengraph-image/route.tsx
@@ -1,17 +1,23 @@
 import { ImageResponse } from "next/og";
+import { APP_ICON_URL } from "~/lib/constants";
 
 export const dynamic = 'force-dynamic';
 
 export async function GET() {
 
+  const imageUrl = APP_ICON_URL;
+
   return new ImageResponse(
     (
-      <div tw="flex h-full w-full flex-col justify-center items-center relative bg-primary">
+      <div tw="flex h-full w-full flex-col justify-center items-center relative bg-[#facc15]">
         <div tw="flex w-96 h-96 rounded-full overflow-hidden mb-8 border-8 border-white">
-          {/* <img src="/images/logo.png" alt="Profile" tw="w-full h-full object-cover" /> */}
+           <img src={imageUrl} alt="Profile" tw="w-full h-full object-cover" />
         </div>
         <h1 tw="text-8xl text-white"></h1>
-        <p tw="text-5xl mt-4 text-white opacity-80">Powered by PizzaDao🍕</p>
+        <p tw="text-5xl mt-4 text-center text-white opacity-80">
+          <span>The Pizza faucet</span> <br />
+          <span>Powered by PizzaDao🍕</span>
+        </p>
       </div>
     ),
     {
